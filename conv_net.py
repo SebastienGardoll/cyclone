88002a23ca0eb1e73e498a898c5a216607f8ef8a
Sebastien Gardoll <sebastien.gardoll@ipsl.fr>
2018-11-09 17:24:21 +0100

    implement auc computation

--- conv_net.py
+++ conv_net.py
@@ -16,7 +16,10 @@ import common
 import sys
 
 import numpy as np
+
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
 
 import keras
 from keras import backend as K
@@ -24,19 +27,24 @@ from keras.models import Model
 from keras.layers import Dense, Input, Conv2D, MaxPooling2D, Flatten
 from keras.utils import plot_model
 
+import time
+start = time.time()
+
                            ####### SETTINGS #######
 
-file_prefix = '2000_10'
+file_prefix = '2k'
 num_core    = 0
 num_classes = common.NUM_CLASSES
 max_mem     = -1
 
 # TODO optimize settings.
-batch_size  = 32
-epochs      = 10
+batch_size  = 5
+epochs      = 100
 loss        = keras.losses.binary_crossentropy # https://keras.io/losses/
 metrics     = ['accuracy']
 optimizer   = keras.optimizers.SGD() # https://keras.io/optimizers/
+#optimizer   = keras.optimizers.Adadelta()
+test_ratio  = 0.3
 
 config = K.tf.ConfigProto()
 
@@ -66,23 +74,26 @@ labels_file_path = path.join(common.TENSOR_PARENT_DIR_PATH, labels_filename)
 
 print(f'> loading labels {labels_filename}')
 labels = np.load(file=labels_file_path, mmap_mode=None, allow_pickle=True)
-labels = keras.utils.to_categorical(labels, num_classes)
 
 process = psutil.Process(os.getpid())
 current_mem = process.memory_info().rss/common.MEGA_BYTES_FACTOR
 if current_mem > max_mem:
   max_mem = current_mem
-
 
 print('> making test and training datasets')
-x_train, x_test, y_train, y_test = train_test_split(tensor, labels, test_size=0.3)
+x_train, x_test, y_train_not_cat, y_test_not_cat = train_test_split(tensor,
+                                                  labels, test_size=test_ratio)
 del tensor, labels
+
+y_train = keras.utils.to_categorical(y_train_not_cat, num_classes)
+y_test  = keras.utils.to_categorical(y_test_not_cat, num_classes)
+del y_train_not_cat
+
 
 process = psutil.Process(os.getpid())
 current_mem = process.memory_info().rss/common.MEGA_BYTES_FACTOR
 if current_mem > max_mem:
   max_mem = current_mem
-
 
                     ####### BUILDING CONV NET #######
 
@@ -107,6 +118,8 @@ model = Model(inputs=input_layer, outputs=fully2_layer)
 print('> compiling the layers')
 model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
 
+print(model.summary())
+
 cnn_architecture_filename  = 'cnn_layers.png'
 cnn_architecture_file_path = path.join(common.CNN_PARENT_DIR_PATH,
                                        cnn_architecture_filename)
@@ -115,16 +128,34 @@ plot_model(model, to_file=cnn_architecture_file_path, show_shapes=True,
            show_layer_names=True)
 
                       ####### FITTING MODEL #######
+
 print('> fitting the model')
 model.fit(x=x_train, y=y_train, epochs=epochs, batch_size=batch_size)
 
-print('> evaluating the model')
+print('> evaluating the model (keras method)')
 loss, metric = model.evaluate(x=x_test, y=y_test, verbose=1)
 print(f'  > loss = {loss}')
 print(f'  > metric = {metric}')
+
+print('> computing AUC')
+
+y_pred = model.predict(x_test, verbose=1)
+# Return the class (0 for no cyclone, 1 for cyclone).
+y_pred = np.argmax(y_pred, axis=1)
+fpr_keras, tpr_keras, thresholds_keras = roc_curve(y_test_not_cat, y_pred)
+auc_model = auc(fpr_keras, tpr_keras)
+print(f'  > {auc_model}')
+
+model_filename  = 'model.h5'
+model_file_path = path.join(common.CNN_PARENT_DIR_PATH, model_filename)
+print(f'> saving the model ({model_filename})')
+model.save(model_file_path)
 
 process = psutil.Process(os.getpid())
 current_mem = process.memory_info().rss/common.MEGA_BYTES_FACTOR
 if current_mem > max_mem:
   max_mem = current_mem
 print(f'> maximum memory footprint: {max_mem:.2f} MiB')
+
+stop = time.time()
+print(f'> spend {(stop-start):.2f} seconds processing')
