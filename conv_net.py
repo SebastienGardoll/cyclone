883556074c5ca350177e9d98bd1062bb200a00a6
Sebastien Gardoll <sebastien.gardoll@ipsl.fr>
2018-11-09 11:56:57 +0100

    implement testing

--- conv_net.py
+++ conv_net.py
@@ -5,14 +5,18 @@ Created on Thu Nov  8 15:51:31 2018
 
 @author: Sébastien Gardoll
 """
+import os
 
 import os.path as path
+
+import psutil
 
 import common
 
 import sys
 
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 import keras
 from keras import backend as K
@@ -22,9 +26,10 @@ from keras.utils import plot_model
 
                            ####### SETTINGS #######
 
-file_prefix = '2k'
+file_prefix = '2000_10'
 num_core    = 0
 num_classes = common.NUM_CLASSES
+max_mem     = -1
 
 # TODO optimize settings.
 batch_size  = 32
@@ -46,26 +51,42 @@ keras.backend.set_image_data_format('channels_last')
 if (len(sys.argv) > 1) and (sys.argv[1].strip()):
   file_prefix = sys.argv[1].strip()
 
-
                          ####### LOADING DATA #######
 
-tensor_filename = f'{common.SHUFFLED_FILE_PREFIX}_{file_prefix}_{common.SHUFFLED_TENSOR_FILE_POSTFIX}.npy'
+tensor_filename = f'{common.SHUFFLED_FILE_PREFIX}_{file_prefix}_\
+{common.SHUFFLED_TENSOR_FILE_POSTFIX}.npy'
 tensor_file_path = path.join(common.TENSOR_PARENT_DIR_PATH, tensor_filename)
 
 print(f'> loading tensor {tensor_filename}')
 tensor = np.load(file=tensor_file_path, mmap_mode=None, allow_pickle=True)
 
-labels_filename = f'{common.SHUFFLED_FILE_PREFIX}_{file_prefix}_{common.SHUFFLED_LABELS_FILE_POSTFIX}.npy'
+labels_filename = f'{common.SHUFFLED_FILE_PREFIX}_{file_prefix}_\
+{common.SHUFFLED_LABELS_FILE_POSTFIX}.npy'
 labels_file_path = path.join(common.TENSOR_PARENT_DIR_PATH, labels_filename)
 
 print(f'> loading labels {labels_filename}')
 labels = np.load(file=labels_file_path, mmap_mode=None, allow_pickle=True)
-
 labels = keras.utils.to_categorical(labels, num_classes)
+
+process = psutil.Process(os.getpid())
+current_mem = process.memory_info().rss/common.MEGA_BYTES_FACTOR
+if current_mem > max_mem:
+  max_mem = current_mem
+
+
+print('> making test and training datasets')
+x_train, x_test, y_train, y_test = train_test_split(tensor, labels, test_size=0.3)
+del tensor, labels
+
+process = psutil.Process(os.getpid())
+current_mem = process.memory_info().rss/common.MEGA_BYTES_FACTOR
+if current_mem > max_mem:
+  max_mem = current_mem
+
 
                     ####### BUILDING CONV NET #######
 
-input_layer    = Input(shape=tensor.shape[1:], name='input')
+input_layer    = Input(shape=x_train.shape[1:], name='input')
 
 conv1_layer    = Conv2D(filters=8, kernel_size=(5, 5), activation='relu',
                         name='conv1')(input_layer)
@@ -94,6 +115,16 @@ plot_model(model, to_file=cnn_architecture_file_path, show_shapes=True,
            show_layer_names=True)
 
                       ####### FITTING MODEL #######
+print('> fitting the model')
+model.fit(x=x_train, y=y_train, epochs=epochs, batch_size=batch_size)
 
-model.fit(x=tensor, y=labels, epochs=epochs, batch_size=batch_size)
+print('> evaluating the model')
+loss, metric = model.evaluate(x=x_test, y=y_test, verbose=1)
+print(f'  > loss = {loss}')
+print(f'  > metric = {metric}')
 
+process = psutil.Process(os.getpid())
+current_mem = process.memory_info().rss/common.MEGA_BYTES_FACTOR
+if current_mem > max_mem:
+  max_mem = current_mem
+print(f'> maximum memory footprint: {max_mem:.2f} MiB')
