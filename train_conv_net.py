01fcdd3d3a558e92a7ffbd68cdd12c2f8223d338
Sebastien Gardoll <sebastien.gardoll@ipsl.fr>
2019-01-25 15:51:53 +0100

    save predictions ; rename *not_cat into class

--- train_conv_net.py
+++ train_conv_net.py
@@ -94,12 +94,12 @@ if current_mem > max_mem:
 
 print('> making test and training datasets')
 # Split arrays or matrices into random train and test subsets.
-x_train, x_test, y_train_not_cat, y_test_not_cat = train_test_split(tensor,
+x_train, x_test, y_train_class, y_test_class = train_test_split(tensor,
                                                   labels, test_size=test_ratio)
 del tensor, labels
 
-y_train = keras.utils.to_categorical(y_train_not_cat, num_classes)
-y_test  = keras.utils.to_categorical(y_test_not_cat, num_classes)
+y_train_cat = keras.utils.to_categorical(y_train_class, num_classes)
+y_test_cat  = keras.utils.to_categorical(y_test_class, num_classes)
 
 print('> saving the datasets')
 
@@ -109,7 +109,7 @@ common.write_ndarray_to_hdf5(file_path, x_train)
 
 file_name = f'train_{file_prefix}_{common.SHUFFLED_LABELS_FILE_POSTFIX}.h5'
 file_path = path.join(common.TENSOR_PARENT_DIR_PATH, file_name)
-common.write_ndarray_to_hdf5(file_path, y_train_not_cat)
+common.write_ndarray_to_hdf5(file_path, y_train_class)
 
 file_name = f'test_{file_prefix}_{common.SHUFFLED_TENSOR_FILE_POSTFIX}.h5'
 file_path = path.join(common.TENSOR_PARENT_DIR_PATH, file_name)
@@ -117,7 +117,7 @@ common.write_ndarray_to_hdf5(file_path, x_test)
 
 file_name = f'test_{file_prefix}_{common.SHUFFLED_LABELS_FILE_POSTFIX}.h5'
 file_path = path.join(common.TENSOR_PARENT_DIR_PATH, file_name)
-common.write_ndarray_to_hdf5(file_path, y_test_not_cat)
+common.write_ndarray_to_hdf5(file_path, y_test_class)
 
 del file_name
 del file_path
@@ -164,10 +164,10 @@ plot_model(model, to_file=cnn_architecture_file_path, show_shapes=True,
 # examples per second: https://github.com/tensorflow/models/blob/master/tutorials/image/cifar10/cifar10_train.py
 
 print('> fitting the model')
-model.fit(x=x_train, y=y_train, epochs=epochs, batch_size=batch_size, verbose=2)
+model.fit(x=x_train, y=y_train_cat, epochs=epochs, batch_size=batch_size, verbose=2)
 
 print('> evaluating the model (keras method)')
-loss, metric = model.evaluate(x=x_test, y=y_test, verbose=1)
+loss, metric = model.evaluate(x=x_test, y=y_test_cat, verbose=1)
 print(f'  > loss = {loss}')
 print(f'  > metric = {metric}')
 
@@ -176,21 +176,32 @@ print('> computing AUC')
 y_pred_raw = model.predict(x_test, verbose=1)
 
 # Return the class (0 for no cyclone, 1 for cyclone).
-y_pred_cat = np.argmax(y_pred_raw, axis=1)
+y_pred_class = np.argmax(y_pred_raw, axis=1)
 
 # Keep only the probabilities.
 y_pred_prob = np.delete(y_pred_raw, obj=0, axis=1).squeeze()
 
-auc_model = roc_auc_score(y_true=y_test_not_cat, y_score=y_pred_prob)
+auc_model = roc_auc_score(y_true=y_test_class, y_score=y_pred_prob)
 print(f'  > {auc_model}')
 
 print('  > displaying the classification report')
-print(classification_report(y_true=y_test_not_cat, y_pred=y_pred_cat, target_names=('no_cyclones', 'cyclones')))
+print(classification_report(y_true=y_test_class, y_pred=y_pred_class, target_names=('no_cyclones', 'cyclones')))
 
 model_filename  = f'{file_prefix}_model.h5'
 model_file_path = path.join(common.CNN_PARENT_DIR_PATH, model_filename)
 print(f'> saving the model ({model_filename})')
 model.save(model_file_path)
+
+print(f'> saving the predictions')
+file_name = f'prediction_class_{file_prefix}_{common.SHUFFLED_LABELS_FILE_POSTFIX}.h5'
+file_path = path.join(common.TENSOR_PARENT_DIR_PATH, file_name)
+common.write_ndarray_to_hdf5(file_path, y_pred_class)
+file_name = f'prediction_prob_{file_prefix}_{common.SHUFFLED_LABELS_FILE_POSTFIX}.h5'
+file_path = path.join(common.TENSOR_PARENT_DIR_PATH, file_name)
+common.write_ndarray_to_hdf5(file_path, y_pred_prob)
+
+del file_name
+del file_path
 
 process = psutil.Process(os.getpid())
 current_mem = process.memory_info().rss/common.MEGA_BYTES_FACTOR
