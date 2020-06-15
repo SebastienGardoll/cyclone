ec7911c99f3d8492ee57b08d9b5b3e0c806664a7
Sébastien Gardoll <sebastien@gardoll.fr>
2020-06-15 14:47:15 +0200

    fix warnings

--- build_cyclone_db.py
+++ build_cyclone_db.py
@@ -18,14 +18,15 @@ start = time.time()
 CYCLONE_HEADER_PATTERN = re.compile('^([A-Z0-9]+), +[-\w]+, +(\d+),$')
 CYCLONE_DESC_PATTERN = re.compile('^(\d{4})(\d{2})(\d{2}), (\d{4}), ([ A-Z]), ([A-Z]{2}), +(\d+\.\d+)([NS]), +(\d+\.\d+)([WE]), +(-?[\d ]+), +(-?[\d ]+).+$')
 
-HOUR_MAPPING = {'0000':0, '0600':6, '1200':12, '1800':18}
+HOUR_MAPPING = {'0000': 0, '0600': 6, '1200': 12, '1800': 18}
 
-CYCLONE_DF_COLUMNS      = ['cyclone_id', 'hurdat2_id', 'year', 'month', 'day',
-                           'hour', 'status', 'lat', 'lon', 'max_sustained_wind', 'min_pressure']
+CYCLONE_DF_COLUMNS = ['cyclone_id', 'hurdat2_id', 'year', 'month', 'day',
+                      'hour', 'status', 'lat', 'lon', 'max_sustained_wind', 'min_pressure']
 
 ERA5_MIN_YEAR = 1979
 
 ERA5_COORDINATES_RESOLUTION = 0.25
+
 
 def display_duration(time_in_sec):
     remainder = time_in_sec % 60
@@ -48,23 +49,24 @@ def display_duration(time_in_sec):
                 hours = remainder
                 return f'{days} days, {hours} hours, {minutes} mins, {seconds:.2f} seconds'
 
+
 def parse_hour(hour_literal):
     try:
         result = HOUR_MAPPING[hour_literal]
         return result
     except KeyError:
-        logging.error("unsupported hour ('%s')"%(hour_literal))
+        logging.error("unsupported hour ('%s')" % hour_literal)
         return None
 
 
 def extract_header(line):
     match = CYCLONE_HEADER_PATTERN.match(line)
     if match:
-        hurdat_id = match.group(1)
-        nb_lines = match.group(2)
-        return (hurdat_id, int(nb_lines))
+        local_hurdat_id = match.group(1)
+        local_nb_lines = match.group(2)
+        return local_hurdat_id, int(local_nb_lines)
     else:
-        raise Exception("unsupported header ('%s')"%(line))
+        raise Exception("unsupported header ('%s')" % line)
 
 
 def extract_record(line, cyclone_id, hurdat2_id):
@@ -99,125 +101,127 @@ def extract_record(line, cyclone_id, hurdat2_id):
             values = [cyclone_id, hurdat2_id, year, month, day, hour, status,
                       latitude, longitude, max_sustained_wind, min_pressure]
             return values
-        else: # skip
+        else:  # skip
             return None
     else:
-        logging.error("unsupported record ('%s')"%(line))
+        logging.error("unsupported record ('%s')" % line)
         return None
 
 
-# Default values
-dataset_parent_dir_path = '/data/sgardoll/cyclone_data/dataset'
-hurdat2_file_path = path.join(dataset_parent_dir_path,
-                              "hurdat2-1851-2019-052520.txt")
+def main() -> None:
+    # Default values
+    dataset_parent_dir_path = '/data/sgardoll/cyclone_data/dataset'
+    hurdat2_file_path = path.join(dataset_parent_dir_path,
+                                  "hurdat2-1851-2019-052520.txt")
 
-if (len(sys.argv) > 1) and (sys.argv[1].strip()):
-    hurdat2_file_path = sys.argv[1].strip()
-    dataset_parent_dir_path = path.dirname(hurdat2_file_path)
+    if (len(sys.argv) > 1) and (sys.argv[1].strip()):
+        hurdat2_file_path = sys.argv[1].strip()
+        dataset_parent_dir_path = path.dirname(hurdat2_file_path)
 
-hurdat2_file = open(hurdat2_file_path, 'r')
-cyclone_list = []
+    hurdat2_file = open(hurdat2_file_path, 'r')
+    cyclone_list = []
 
-lines = hurdat2_file.readlines()
-cyclone_id = 0
-index = 0
-skipped_row_count = 0
-row_count = 0
-current_year  = -1
-previous_year = -1
+    lines = hurdat2_file.readlines()
+    cyclone_id = 0
+    index = 0
+    skipped_row_count = 0
+    row_count = 0
+    current_year  = -1
+    previous_year = -1
 
-print(f'> starting to process {hurdat2_file_path}')
-while index < len(lines):
-    current_line = lines[index]
-    index = index + 1
-    (hurdat_id, nb_lines) = extract_header(current_line)
-    first_record_line = row_count
-    for index in range(index, (index + nb_lines)):
+    print(f'> starting to process {hurdat2_file_path}')
+    while index < len(lines):
         current_line = lines[index]
-        cyclone_values = extract_record(current_line, cyclone_id, hurdat_id)
-        if cyclone_values is not None:
-            cyclone_list.append(cyclone_values)
-            row_count = row_count + 1
-            current_year = cyclone_values[2]
-            if current_year != previous_year:
-                print(f'  > processing year: {current_year}')
-                previous_year = current_year
-        else:
-            skipped_row_count = skipped_row_count + 1
-    last_record_line_plus_1 = row_count
-    mapping_values = [cyclone_id, hurdat_id, first_record_line, last_record_line_plus_1]
-    cyclone_id = cyclone_id + 1
-    index = index + 1
+        index = index + 1
+        (hurdat_id, nb_lines) = extract_header(current_line)
+        for index in range(index, (index + nb_lines)):
+            current_line = lines[index]
+            cyclone_values = extract_record(current_line, cyclone_id, hurdat_id)
+            if cyclone_values is not None:
+                cyclone_list.append(cyclone_values)
+                row_count = row_count + 1
+                current_year = cyclone_values[2]
+                if current_year != previous_year:
+                    print(f'  > processing year: {current_year}')
+                    previous_year = current_year
+            else:
+                skipped_row_count = skipped_row_count + 1
+        cyclone_id = cyclone_id + 1
+        index = index + 1
 
-cyclone_dataframe = pd.DataFrame(data=cyclone_list, columns=CYCLONE_DF_COLUMNS)
+    cyclone_dataframe = pd.DataFrame(data=cyclone_list, columns=CYCLONE_DF_COLUMNS)
 
-print(f'> number of row skipped: {skipped_row_count}')
-# 19 002
+    print(f'> number of row skipped: {skipped_row_count}')
+    # 19 002
 
-hurdat2_file.close()
+    hurdat2_file.close()
 
-print('> sorting cyclone dataset according to the date')
-cyclone_dataframe.sort_values(by=['year', 'month', 'day', 'hour'],
-                              ascending=True, inplace=True)
+    print('> sorting cyclone dataset according to the date')
+    cyclone_dataframe.sort_values(by=['year', 'month', 'day', 'hour'],
+                                  ascending=True, inplace=True)
 
-print(f'> drop cyclones before {ERA5_MIN_YEAR-1} included (ERA5 specs)')
-dropped_indexes = cyclone_dataframe[cyclone_dataframe['year'] < ERA5_MIN_YEAR].index
-cyclone_dataframe.drop(dropped_indexes, inplace=True)
+    print(f'> drop cyclones before {ERA5_MIN_YEAR-1} included (ERA5 specs)')
+    dropped_indexes = cyclone_dataframe[cyclone_dataframe['year'] < ERA5_MIN_YEAR].index
+    cyclone_dataframe.drop(dropped_indexes, inplace=True)
+
+    print('> rebuilding the index of the cyclone dataset')
+    cyclone_dataframe = cyclone_dataframe.reset_index(drop=True)
+
+    print('> translating into ERA5 coordinate systems')
+    import nxtensor.utils.coordinate_utils as cu
+    from nxtensor.utils.coordinates import CoordinateFormat
+    cu.reformat_coordinates(cyclone_dataframe, 'lat', CoordinateFormat.INCREASING_DEGREE_NORTH,
+                            CoordinateFormat.DECREASING_DEGREE_NORTH, ERA5_COORDINATES_RESOLUTION, 2)
+    cu.reformat_coordinates(cyclone_dataframe, 'lon', CoordinateFormat.M_180_TO_180_DEGREE_EAST,
+                            CoordinateFormat.ZERO_TO_360_DEGREE_EAST, ERA5_COORDINATES_RESOLUTION, 2)
+
+    filename = 'all_cyclone_dataset.csv'
+    print(f'> saving {filename} ({len(cyclone_dataframe)} rows)')
+    cyclone_dataframe_file_path = path.join(dataset_parent_dir_path, filename)
+    cyclone_dataframe.to_csv(cyclone_dataframe_file_path, sep=',', na_rep='', header=True,
+                             index=True, index_label='img_id', encoding='utf8', line_terminator='\n')
+    # all_extraction.shape # (9684, 11)
+
+    # Extraction of post 2000 records.
+    extraction_2ka = cyclone_dataframe[cyclone_dataframe['year'] >= 2000]
+    filename = '2ka_cyclone_dataset.csv'
+    print(f'> saving {filename} ({len(extraction_2ka)} rows)')
+    extraction_2ka_file_path = path.join(dataset_parent_dir_path, filename)
+    extraction_2ka.to_csv(extraction_2ka_file_path, sep=',', na_rep='', header=True,
+                          index=True, index_label='img_id', encoding='utf8', line_terminator='\n')
+    # extraction_2k.shape # (5520, 11)
+
+    # Extraction of post 2000 records except August of 2000.
+    extraction_2kb = cyclone_dataframe[(cyclone_dataframe.year >= 2000) &
+                                       (~ ((cyclone_dataframe.year == 2000) & (cyclone_dataframe.month == 8)))]
+    filename = '2kb_cyclone_dataset.csv'
+    print(f'> saving {filename} ({len(extraction_2kb)} rows)')
+    extraction_2kb_file_path = path.join(dataset_parent_dir_path, filename)
+    extraction_2kb.to_csv(extraction_2kb_file_path, sep=',', na_rep='', header=True,
+                          index=True, index_label='img_id', encoding='utf8', line_terminator='\n')
+    # extraction_2kb.shape # (5421, 11)
+
+    year_extraction = extraction_2ka[extraction_2ka['year'] < 2001]
+    filename = '2000_cyclone_dataset.csv'
+    print(f'> saving {filename} ({len(year_extraction)} rows)')
+    year_extraction_file_path = path.join(dataset_parent_dir_path, filename)
+    year_extraction.to_csv(year_extraction_file_path, sep=',', na_rep='', header=True,
+                           index=True, index_label='img_id', encoding='utf8', line_terminator='\n')
+    # year_extraction.shape # (265, 11)
+
+    month_extraction = year_extraction[year_extraction['month'] == 10]
+    filename = '2000_10_cyclone_dataset.csv'
+    print(f'> saving {filename} ({len(month_extraction)} rows)')
+    month_extraction_file_path = path.join(dataset_parent_dir_path, filename)
+    month_extraction.to_csv(month_extraction_file_path, sep=',', na_rep='', header=True,
+                            index=True, index_label='img_id', encoding='utf8', line_terminator='\n')
+    # month_extraction.shape # (49, 11)
+
+    stop = time.time()
+    formatted_time = display_duration((stop-start))
+    print(f'> spend {formatted_time} processing')
 
 
-print('> rebuilding the index of the cyclone dataset')
-cyclone_dataframe = cyclone_dataframe.reset_index(drop=True)
-
-print('> translating into ERA5 coordinate systems')
-import nxtensor.utils.coordinate_utils as cu
-from nxtensor.utils.coordinates import CoordinateFormat
-cu.reformat_coordinates(cyclone_dataframe, 'lat', CoordinateFormat.INCREASING_DEGREE_NORTH,
-                        CoordinateFormat.DECREASING_DEGREE_NORTH, ERA5_COORDINATES_RESOLUTION, 2)
-cu.reformat_coordinates(cyclone_dataframe, 'lon', CoordinateFormat.M_180_TO_180_DEGREE_EAST,
-                        CoordinateFormat.ZERO_TO_360_DEGREE_EAST, ERA5_COORDINATES_RESOLUTION, 2)
-
-filename = 'all_cyclone_dataset.csv'
-print(f'> saving {filename} ({len(cyclone_dataframe)} rows)')
-cyclone_dataframe_file_path = path.join(dataset_parent_dir_path, filename)
-cyclone_dataframe.to_csv(cyclone_dataframe_file_path, sep = ',', na_rep = '', header = True,
-                         index = True, index_label='img_id', encoding = 'utf8', line_terminator = '\n')
-# all_extraction.shape # (9684, 11)
-
-# Extraction of post 2000 records.
-extraction_2ka = cyclone_dataframe[cyclone_dataframe['year'] >= 2000]
-filename = '2ka_cyclone_dataset.csv'
-print(f'> saving {filename} ({len(extraction_2ka)} rows)')
-extraction_2ka_file_path = path.join(dataset_parent_dir_path, filename)
-extraction_2ka.to_csv(extraction_2ka_file_path, sep = ',', na_rep = '', header = True,
-                      index = True, index_label='img_id', encoding = 'utf8', line_terminator = '\n')
-#extraction_2k.shape # (5520, 11)
-
-# Extraction of post 2000 records except August of 2000.
-extraction_2kb = cyclone_dataframe[(cyclone_dataframe.year >= 2000) &
-                                   (~ ((cyclone_dataframe.year == 2000) & (cyclone_dataframe.month == 8)))]
-filename = '2kb_cyclone_dataset.csv'
-print(f'> saving {filename} ({len(extraction_2kb)} rows)')
-extraction_2kb_file_path = path.join(dataset_parent_dir_path, filename)
-extraction_2kb.to_csv(extraction_2kb_file_path, sep = ',', na_rep = '', header = True,
-                      index = True, index_label='img_id', encoding = 'utf8', line_terminator = '\n')
-#extraction_2kb.shape # (5421, 11)
-
-year_extraction = extraction_2ka[extraction_2ka['year'] < 2001]
-filename = '2000_cyclone_dataset.csv'
-print(f'> saving {filename} ({len(year_extraction)} rows)')
-year_extraction_file_path = path.join(dataset_parent_dir_path, filename)
-year_extraction.to_csv(year_extraction_file_path, sep = ',', na_rep = '', header = True,
-                       index = True, index_label='img_id', encoding = 'utf8', line_terminator = '\n')
-#year_extraction.shape # (265, 11)
-
-month_extraction = year_extraction[year_extraction['month'] == 10]
-filename = '2000_10_cyclone_dataset.csv'
-print(f'> saving {filename} ({len(month_extraction)} rows)')
-month_extraction_file_path = path.join(dataset_parent_dir_path, filename)
-month_extraction.to_csv(month_extraction_file_path, sep = ',', na_rep = '', header = True,
-                        index = True, index_label='img_id', encoding = 'utf8', line_terminator = '\n')
-#month_extraction.shape # (49, 11)
-
-stop = time.time()
-formatted_time =display_duration((stop-start))
-print(f'> spend {formatted_time} processing')
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
--- build_no_cyclone_db.py
+++ build_no_cyclone_db.py
@@ -18,13 +18,13 @@ import time
 start = time.time()
 
 # Default value.
-FILE_PREFIX = '2000_10'
+file_prefix = '2000_10'
 
 if (len(sys.argv) > 1) and (sys.argv[1].strip()):
-  FILE_PREFIX = sys.argv[1].strip()
+    file_prefix = sys.argv[1].strip()
 
 cyclone_db_file_path = path.join(common.DATASET_PARENT_DIR_PATH,
-                          f'{FILE_PREFIX}_{common.CYCLONE_DB_FILE_POSTFIX}.csv')
+                                 f'{file_prefix}_{common.CYCLONE_DB_FILE_POSTFIX}.csv')
 cyclone_db_file = open(cyclone_db_file_path, 'r')
 CYCLONE_DATAFRAME = pd.read_csv(cyclone_db_file, sep=',', header=0, index_col=0,
                                 na_values='')
@@ -41,84 +41,87 @@ NO_CYCLONE_DF_COLUMNS = {'year': np.int16,
 
 
 def _has_cyclone(date, hour, lat, lon):
-  records = CYCLONE_DATAFRAME.query(f'year=={date.year} and\
+    records = CYCLONE_DATAFRAME.query(f'year=={date.year} and\
                                       month=={date.month}\
                                       and day=={date.day} and\
                                       hour=={hour}')
-  if not records.empty:
-    for (index, record) in records.iterrows():
-      lat2 = record["lat"]
-      lon2 = record["lon"]
-      if common.is_overlapping(lat, lon, lat2, lon2):
-        return True
-    return False
-  else:
-    return False
+    if not records.empty:
+        for (index, record) in records.iterrows():
+            lat2 = record["lat"]
+            lon2 = record["lon"]
+            if common.is_overlapping(lat, lon, lat2, lon2):
+                return True
+        return False
+    else:
+        return False
 
 
-def compute_no_cyclone(time, delta):
-  (year, month, day, hour, lat, lon) = time
-  past = common.subtract_delta(year, month, day, delta)
-  has_cyclone = _has_cyclone(past, hour, lat, lon)
-  while has_cyclone:
-    hour = hour - 6
-    if hour < 0:
-      hour = int((common.HOURLY_TIME_SAMPLING / common._4XDAILY_TIME_SAMPLING) * 3)
-      past = common._subtract_one_day(past)
+def compute_no_cyclone(time_tuple, delta):
+    (year, month, day, hour, lat, lon) = time_tuple
+    past = common.subtract_delta(year, month, day, delta)
     has_cyclone = _has_cyclone(past, hour, lat, lon)
-  return (past.year, past.month, past.day, hour, lat, lon)
+    while has_cyclone:
+        hour = hour - 6
+        if hour < 0:
+            hour = int((common.HOURLY_TIME_SAMPLING / common._4XDAILY_TIME_SAMPLING) * 3)
+            past = common._subtract_one_day(past)
+        has_cyclone = _has_cyclone(past, hour, lat, lon)
+    return past.year, past.month, past.day, hour, lat, lon
 
-                            ######## MAIN ########
+
+def main():
+    no_cyclone_list = []
+    print('> computing the no cyclone records')
+    current_year = -1
+    for (index, row) in CYCLONE_DATAFRAME.iterrows():
+        cyclone_year  = row['year']
+        cyclone_month = row['month']
+        cyclone_day   = row['day']
+        cyclone_hour  = row['hour']
+        cyclone_lat   = row['lat']
+        cyclone_lon   = row['lon']
+        if current_year != cyclone_year:
+            current_year = cyclone_year
+            print(f'  > compute year: {current_year}')
+        cyclone_values = (cyclone_year, cyclone_month, cyclone_day,
+                          cyclone_hour, cyclone_lat, cyclone_lon)
+        no_cyclone_list.append(compute_no_cyclone(cyclone_values, common.ONE_DAY))
+        no_cyclone_list.append(compute_no_cyclone(cyclone_values, common.ONE_WEEK))
+
+    # Appending rows one by one in the while loop takes far more time then this.
+    no_cyclone_dataframe = pd.DataFrame(data=no_cyclone_list,
+                                        columns=list(NO_CYCLONE_DF_COLUMNS.keys()))
+    # Specify the schema.
+    no_cyclone_dataframe = no_cyclone_dataframe.astype(dtype=NO_CYCLONE_DF_COLUMNS)
+
+    # Remove duplicated rows.
+    print(f'> number of records before removing the duplicates: {len(no_cyclone_dataframe)}')
+    no_cyclone_dataframe = no_cyclone_dataframe.drop_duplicates()
+    print(f'> number of records  AFTER removing the duplicates: {len(no_cyclone_dataframe)}')
+
+    # Sort by date (month) (optimize building channels)
+    print('> sorting the rows')
+    no_cyclone_dataframe.sort_values(by=['year', 'month', 'day', 'hour'],
+                                     ascending=True, inplace=True)
+
+    # Rebuild the ids of the dataframe.
+    print('> rebuilding the index of the dataframe')
+    no_cyclone_dataframe = no_cyclone_dataframe.reset_index(drop=True)
+
+    filename = f'{file_prefix}_{common.NO_CYCLONE_DB_FILE_POSTFIX}.csv'
+    print(f'> saving the {filename} on disk')
+    no_cyclone_dataframe_file_path = path.join(common.DATASET_PARENT_DIR_PATH,
+                                               filename)
+    no_cyclone_dataframe.to_csv(no_cyclone_dataframe_file_path, sep=',',
+                                na_rep='', header=True, index=True,
+                                index_label='id', encoding='utf8',
+                                line_terminator='\n')
+
+    stop = time.time()
+    formatted_time = common.display_duration((stop-start))
+    print(f'> spend {formatted_time} processing')
 
 
-no_cyclone_list = []
-
-print('> computing the no cyclone records')
-current_year = -1
-for (index, row) in CYCLONE_DATAFRAME.iterrows():
-  cyclone_year  = row['year']
-  cyclone_month = row['month']
-  cyclone_day   = row['day']
-  cyclone_hour  = row['hour']
-  cyclone_lat   = row['lat']
-  cyclone_lon   = row['lon']
-  if current_year != cyclone_year:
-    current_year = cyclone_year
-    print(f'  > compute year: {current_year}')
-  cyclone_values = (cyclone_year, cyclone_month, cyclone_day,\
-                    cyclone_hour, cyclone_lat, cyclone_lon)
-  no_cyclone_list.append(compute_no_cyclone(cyclone_values, common.ONE_DAY))
-  no_cyclone_list.append(compute_no_cyclone(cyclone_values, common.ONE_WEEK))
-
-# Appending rows one by one in the while loop takes far more time then this.
-no_cyclone_dataframe = pd.DataFrame(data=no_cyclone_list,
-                                    columns=list(NO_CYCLONE_DF_COLUMNS.keys()))
-# Specify the schema.
-no_cyclone_dataframe = no_cyclone_dataframe.astype(dtype = NO_CYCLONE_DF_COLUMNS)
-
-# Remove duplicated rows.
-print(f'> number of records before removing the duplicates: {len(no_cyclone_dataframe)}')
-no_cyclone_dataframe = no_cyclone_dataframe.drop_duplicates()
-print(f'> number of records  AFTER removing the duplicates: {len(no_cyclone_dataframe)}')
-
-# Sort by date (month) (optimize building channels)
-print('> sorting the rows')
-no_cyclone_dataframe.sort_values(by=['year', 'month', 'day', 'hour'],
-                                 ascending=True, inplace=True)
-
-# Rebuild the ids of the dataframe.
-print('> rebuilding the index of the dataframe')
-no_cyclone_dataframe = no_cyclone_dataframe.reset_index(drop=True)
-
-filename = f'{FILE_PREFIX}_{common.NO_CYCLONE_DB_FILE_POSTFIX}.csv'
-print(f'> saving the {filename} on disk')
-no_cyclone_dataframe_file_path = path.join(common.DATASET_PARENT_DIR_PATH,
-                                           filename)
-no_cyclone_dataframe.to_csv(no_cyclone_dataframe_file_path, sep=',',
-                            na_rep='', header=True, index=True,
-                            index_label='id', encoding='utf8',
-                            line_terminator='\n')
-
-stop = time.time()
-formatted_time =common.display_duration((stop-start))
-print(f'> spend {formatted_time} processing')
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
