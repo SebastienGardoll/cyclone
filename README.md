d3dc5c43ca703c9ecea92e25e8f939b84f13dbb9
Sébastien Gardoll <sebastien@gardoll.fr>
2020-06-18 14:46:01 +0200

    GPU version preparation

--- README.md
+++ README.md
@@ -8,21 +8,32 @@
 
 The tested version in parenthesis.
 
-- Python 3.7 (3.7.7)
+- Python 3.7 (3.7.4)
 - h5py (2.10.0)
-- keras (2.3.1)
+- keras (2.2.4)
+- keras-tuner (1.0.1)
 - numpy (1.18.1)
-- pandas (1.0.3)
+- pandas (1.0.1)
 - scikit-learn (0.22.1)
-- tensorboard (2.2.1)
+- tensorboard (2.0.0)
 - tensorflow (2.0.0)
 
 
-## Conda dependencies installation script
+## Dependencies installation script
+
+### Conda
 
 ```bash
 YOUR_ENV_NAME='gpu_cyclone'
 conda create -n ${YOUR_ENV_NAME} python=3.7  # Wait.
 conda install -n ${YOUR_ENV_NAME} h5py numpy pandas scikit-learn tensorboard tensorflow'>=2'  # Wait.
 source activate ${YOUR_ENV_NAME}
+```
+
+### Pip
+
+```bash
+YOUR_ENV_NAME='gpu_cyclone'
+source activate ${YOUR_ENV_NAME}
+pip install -U keras-tuner
 ```
--- train_conv_net.py
+++ train_conv_net.py
@@ -28,60 +28,60 @@ from tensorflow.keras.layers import Dense, Input, Conv2D, MaxPooling2D, Flatten
 import time
 start = time.time()
 
-# SETTINGS #######
+# ENVIRONMENT #######
+
+print(f'GPU support: {tf.test.is_built_with_gpu_support()}, CUDA support: {tf.test.is_built_with_cuda()}')
+print('GPU availability:')
+tf.test.is_gpu_available()
+
+# CONSTANTS #######
+
+DATE_FORMAT = '%m_%d_%Y_%H-%M-%S'
 
 # Default values:
-prefix = '2ka'
-parent_dir_path = '/data/sgardoll/extractions/2ka_extraction/tensors'
-num_threads = 10  # Zero means that Tensor will determine the number of threads (all the cores ?).
+DEFAULT_PREFIX = 'all'
+DEFAULT_PARENT_DIR_PATH = '/data/sgardoll/extractions/all_extraction/tensors'
 
-if (len(sys.argv) > 3) and (sys.argv[1].strip()) and (sys.argv[2].strip()) and (sys.argv[3].strip()):
-    prefix = sys.argv[1].strip()
-    parent_dir_path = sys.argv[2].strip()
-    num_threads = int(sys.argv[3].strip())
-    print(f'> settings prefix to {prefix}')
-    print(f'> setting parent directory to {parent_dir_path}')
-    print(f'> setting number of core to {num_threads}')
+# SETTINGS #######
+
+if (len(sys.argv) > 2) and (sys.argv[1].strip()) and (sys.argv[2].strip()):
+    data_prefix = sys.argv[1].strip()
+    data_parent_dir_path = sys.argv[2].strip()
+    print(f'> settings prefix to {data_prefix}')
+    print(f'> setting parent directory to {data_parent_dir_path}')
+else:
+    data_prefix = DEFAULT_PREFIX
+    data_parent_dir_path = DEFAULT_PARENT_DIR_PATH
+
+del DEFAULT_PREFIX, DEFAULT_PARENT_DIR_PATH
+
 
 # In general: Larger batch sizes result in faster progress in training, but don't always converge as fast.
 # Smaller batch sizes train slower, but can converge faster. It's definitely problem dependent.
 batch_size = 32 # Default for CPU. # TODO: to be optimzed.
 
-number_epochs = 5  # TODO: to be optimzed.
+number_epochs = 20  # TODO: to be optimzed.
 
 loss = keras.losses.BinaryCrossentropy()  # https://keras.io/losses/
 metrics = ['accuracy']
 
-
-learning_rate = 0.1  # TODO: Learning rate.
+learning_rate = 0.001  # TODO: Learning rate.
 optimizer = keras.optimizers.SGD(learning_rate=learning_rate)  # TODO: choose optimizer.
-
-
-if num_threads > 1:
-    num_threads = num_threads - 1
-
-config = tf.config
-
-# The number of threads created by Tensorflow for independent operations.
-config.threading.set_inter_op_parallelism_threads = num_threads
-
-# The number of threads created by Tensorflow for each operations.
-config.threading.set_intra_op_parallelism_threads = 1
 
 # set data_format to 'channels_last'
 keras.backend.set_image_data_format('channels_last')
 
 # LOADING DATA #######
 
-training_tensor_file_path = path.join(parent_dir_path, f'training_{prefix}_data.h5')
+training_tensor_file_path = path.join(data_parent_dir_path, f'training_{data_prefix}_data.h5')
 training_tensor = common.read_ndarray_from_hdf5(filepath=training_tensor_file_path)
 print(f'training tensor shape: {training_tensor.shape}')
 
-validation_tensor_file_path = path.join(parent_dir_path, f'validation_{prefix}_data.h5')
+validation_tensor_file_path = path.join(data_parent_dir_path, f'validation_{data_prefix}_data.h5')
 validation_tensor = common.read_ndarray_from_hdf5(filepath=validation_tensor_file_path)
 print(f'validation tensor shape: {validation_tensor.shape}')
 
-test_tensor_file_path = path.join(parent_dir_path, f'test_{prefix}_data.h5')
+test_tensor_file_path = path.join(data_parent_dir_path, f'test_{data_prefix}_data.h5')
 test_tensor = common.read_ndarray_from_hdf5(filepath=test_tensor_file_path)
 print(f'test tensor shape: {test_tensor.shape}')
 
@@ -92,17 +92,17 @@ METADATA_TYPES = {'day': np.int8, 'day2d': np.str,
                   'year': np.int16, 'lat': np.float64, 'lon': np.float64,
                   'label_num_id': np.float64}
 
-training_labels_file_path = path.join(parent_dir_path, f'training_{prefix}_metadata.csv')
+training_labels_file_path = path.join(data_parent_dir_path, f'training_{data_prefix}_metadata.csv')
 training_metadata: pd.DataFrame = pd.read_csv(filepath_or_buffer=training_labels_file_path, dtype=METADATA_TYPES)
 training_labels = training_metadata['label_num_id'].to_numpy()
 print(f'training labels shape: {training_labels.shape}')
 
-validation_labels_file_path = path.join(parent_dir_path, f'validation_{prefix}_metadata.csv')
+validation_labels_file_path = path.join(data_parent_dir_path, f'validation_{data_prefix}_metadata.csv')
 validation_metadata: pd.DataFrame = pd.read_csv(filepath_or_buffer=validation_labels_file_path, dtype=METADATA_TYPES)
 validation_labels = validation_metadata['label_num_id'].to_numpy()
 print(f'validation labels shape: {validation_labels.shape}')
 
-test_labels_file_path = path.join(parent_dir_path, f'test_{prefix}_metadata.csv')
+test_labels_file_path = path.join(data_parent_dir_path, f'test_{data_prefix}_metadata.csv')
 test_metadata: pd.DataFrame = pd.read_csv(filepath_or_buffer=test_labels_file_path, dtype=METADATA_TYPES)
 test_labels = test_metadata['label_num_id'].to_numpy()
 print(f'test labels shape: {test_labels.shape}')
@@ -134,12 +134,12 @@ print(model.summary())
 
 # FITTING MODEL #######
 import datetime
-log_dir_path = path.join(parent_dir_path, 'fit_logs', datetime.datetime.now().strftime('%m_%d_%Y_%H-%M-%S'))
+log_dir_path = path.join('fit_logs', datetime.datetime.now().strftime(DATE_FORMAT))
 os.makedirs(log_dir_path, exist_ok=True)
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir_path, histogram_freq=1)
 
 # %load_ext tensorboard
-
+start = time.time()
 print('> fitting the model')
 model.fit(x=training_tensor, y=training_labels, validation_data=(validation_tensor, validation_labels),
           epochs=number_epochs, batch_size=batch_size, verbose=2, callbacks=[tensorboard_callback])
@@ -147,13 +147,13 @@ model.fit(x=training_tensor, y=training_labels, validation_data=(validation_tens
 # %tensorboard --logdir $log_dir_path
 
 print('> evaluating the model on test dataset')
-loss, metric = model.evaluate(x=test_tensor, y=test_labels, verbose=1)
+loss, metric = model.evaluate(x=test_tensor, y=test_labels, verbose=0)
 print(f'  > loss = {loss}')
 print(f'  > metric = {metric}')
 
-test_predicted_probs = model.predict(test_tensor, verbose=1)
+test_predicted_probs = model.predict(test_tensor, verbose=0)
 auc_model = roc_auc_score(y_true=test_labels, y_score=test_predicted_probs)
-print(f'  > {auc_model}')
+print(f'  > roc = {auc_model}')
 
 
 # Convert the probabilities into the class based on the higher probability.
@@ -161,11 +161,11 @@ print(f'  > {auc_model}')
 threshold_probability = 0.5
 test_predicted_class = np.where(test_predicted_probs > threshold_probability, 1, 0)
 
-print('  > displaying the classification report')
-print(classification_report(y_true=test_labels, y_pred=test_predicted_class, target_names=('no_cyclones', 'cyclones')))
+print('  > the classification report:')
+print(classification_report(y_true=test_labels, y_pred=test_predicted_class, target_names=['no_cyclones', 'cyclones']))
 
-model_filename = f'{prefix}_model.h5'
-model_file_path = path.join(parent_dir_path, '../cnn', model_filename)
+model_filename = f'{data_prefix}_model_{datetime.datetime.now().strftime(DATE_FORMAT)}.h5'
+model_file_path = path.join('cnn', model_filename)
 os.makedirs(path.dirname(model_file_path), exist_ok=True)
 print(f'> saving the model ({model_filename})')
 model.save(model_file_path)
