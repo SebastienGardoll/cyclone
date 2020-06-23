0a93af33a1d5b4ad4eed48abed16013e88ab719f
Sébastien Gardoll <sebastien@gardoll.fr>
2020-06-23 12:03:37 +0200

    fix most of warnings

--- predict_cyclone_utils.py
+++ predict_cyclone_utils.py
@@ -13,7 +13,6 @@ import multiprocessing as mp
 import ctypes
 
 import common
-import extraction_utils as ex_utils
 from common import Era5
 
 import csv
@@ -68,6 +67,7 @@ def _normalize_dataset(chan_array, variable, netcdf_dataset, time_index, mean, s
     unsharable_norm_dataset = (data - mean)/stddev
     np.copyto(dst=chan_array, src=unsharable_norm_dataset, casting='no')
 
+
 def extract_region(img_spec, normalized_dataset, channels_array):
     (id, lat_min_idx, lat_max_idx, lon_min_idx, lon_max_idx) = img_spec
     for variable in Era5:
@@ -75,36 +75,55 @@ def extract_region(img_spec, normalized_dataset, channels_array):
         dest_array = channels_array[variable.value.num_id][id]
         np.copyto(dst=dest_array, src=nc_dataset[lat_min_idx:lat_max_idx, lon_min_idx:lon_max_idx], casting='no')
 
+
+def build_dataset_dict(year, month):
+    result = {Era5.MSL: open_netcdf(Era5.MSL, year, month),
+              Era5.U10: open_netcdf(Era5.U10, year, month),
+              Era5.V10: open_netcdf(Era5.V10, year, month),
+              Era5.TCWV: open_netcdf(Era5.TCWV, year, month),
+              Era5.TA200: open_netcdf(Era5.TA200, year, month),
+              Era5.TA500: open_netcdf(Era5.TA500, year, month),
+              Era5.U850: open_netcdf(Era5.U850, year, month),
+              Era5.V850: open_netcdf(Era5.V850, year, month)}
+    return result
+
+
 def open_cyclone_db():
     cyclone_db_file = open(common.CYCLONE_ALL_DB_FILE_PATH, 'r')
     cyclone_dataframe = pd.read_csv(cyclone_db_file, sep=',', header=0, index_col=0, na_values='')
     cyclone_db_file.close()
     return cyclone_dataframe
 
+
 def close_dataset_dict(dataset_dict):
-  for dataset in dataset_dict.values():
-    dataset.close()
+    for dataset in dataset_dict.values():
+        dataset.close()
+
 
 def compute_min_max(lat, lon):
     lat_min = common.round_nearest((lat - common.HALF_LAT_FRAME), common.LAT_RESOLUTION, common.NUM_DECIMAL_LAT)
     lat_max = common.round_nearest((lat + common.HALF_LAT_FRAME), common.LAT_RESOLUTION, common.NUM_DECIMAL_LAT)
     lon_min = common.round_nearest((lon - common.HALF_LON_FRAME), common.LON_RESOLUTION, common.NUM_DECIMAL_LON)
     lon_max = common.round_nearest((lon + common.HALF_LON_FRAME), common.LON_RESOLUTION, common.NUM_DECIMAL_LON)
-    return (lat_min, lat_max, lon_min, lon_max)
+    return lat_min, lat_max, lon_min, lon_max
+
 
 def format_record(idx, record):
     lat = record['lat']
     lon = record['lon']
     (lat_min, lat_max, lon_min, lon_max) = compute_min_max(lat, lon)
-    return f'  > id: {idx} ; lat = {lat} ; lon = {lon} ; lat_min = {lat_min} ; lat_max = {lat_max} ; lon_min = {lon_min} ; lon_max = {lon_max}'
+    return f'  > id: {idx} ; lat = {lat} ; lon = {lon} ; lat_min = {lat_min} ; lat_max = {lat_max} ; ' + \
+           f'lon_min = {lon_min} ; lon_max = {lon_max}'
+
 
 def display_intermediate_time():
     if IS_DEBUG:
         global PREVIOUS_INTERMEDIATE_TIME
         intermediate_time = time.time()
-        formatted_time =common.display_duration((intermediate_time-PREVIOUS_INTERMEDIATE_TIME))
+        formatted_time = common.display_duration((intermediate_time-PREVIOUS_INTERMEDIATE_TIME))
         PREVIOUS_INTERMEDIATE_TIME = intermediate_time
         print(f'  > intermediate processing time: {formatted_time}')
+
 
 def compute_recorded_cyclones(cyclone_dataframe, year, month, day, hour):
     # Check if there is any cyclone for the given settings.
@@ -124,14 +143,16 @@ def compute_recorded_cyclones(cyclone_dataframe, year, month, day, hour):
             print(format_record(idx, record))
 
     display_intermediate_time()
-    return (recorded_cyclones, nb_cyclones)
+    return recorded_cyclones, nb_cyclones
+
 
 def compute_interval(dataframe):
     lat_min = dataframe['lat'].min() - common.LAT_FRAME
     lat_max = dataframe['lat'].max() + common.LAT_FRAME
     lon_min = dataframe['lon'].min() - common.LON_FRAME
     lon_max = dataframe['lon'].max() + common.LON_FRAME
-    return (lat_min, lat_max, lon_min, lon_max)
+    return lat_min, lat_max, lon_min, lon_max
+
 
 def check_interval(dataframe, lat_min, lat_max, lon_min, lon_max):
     df_lat_min, df_lat_max, df_lon_min, df_lon_max = compute_interval(dataframe)
@@ -149,6 +170,7 @@ def check_interval(dataframe, lat_min, lat_max, lon_min, lon_max):
         return False
     return True
 
+
 def open_netcdf_files(year, month):
     print(f'> opening netcdf files (year: {year}; month: {month})')
     netcdf_dict = ex_utils.build_dataset_dict(year, month)
@@ -158,6 +180,7 @@ def open_netcdf_files(year, month):
             break
     display_intermediate_time()
     return (netcdf_dict, shape)
+
 
 def normalize_netcdf(file_prefix, netcdf_dict, shape, day, hour):
     # Allocation of the datasets which values are normalized.
@@ -170,9 +193,9 @@ def normalize_netcdf(file_prefix, netcdf_dict, shape, day, hour):
     print(f'> normalizing netcdf files ({stats_filename})')
     stats_dataframe_file_path = path.join(common.MERGED_CHANNEL_PARENT_DIR_PATH,
                                           stats_filename)
-    with open (stats_dataframe_file_path, 'r') as csv_file:
+    with open(stats_dataframe_file_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',', lineterminator='\n')
-        next(csv_reader) # Skip the header.
+        next(csv_reader)  # Skip the header.
         # Statistics are ordered the same as the ids of ERA5 enums.
         for variable in Era5:
             (mean, stddev) = next(csv_reader)
@@ -184,6 +207,7 @@ def normalize_netcdf(file_prefix, netcdf_dict, shape, day, hour):
                                time_index, mean, stddev)
     display_intermediate_time()
     return normalized_dataset
+
 
 def compute_chunks(lat_min, lat_max, lon_min, lon_max):
     # Round lat&lon so as to compute synchronize with ERA5 resolution.
@@ -229,7 +253,7 @@ def compute_chunks(lat_min, lat_max, lon_min, lon_max):
     index_list = []
     image_list = []
     current_lat_min_idx = lat_min_idx
-    current_lat_max     = rounded_lat_max # Latitude indexes are inverted.
+    current_lat_max     = rounded_lat_max  # Latitude indexes are inverted.
     while current_lat_min_idx < lat_max_idx:
         current_lat_max_idx = current_lat_min_idx + common.Y_RESOLUTION
         current_lon_min_idx = lon_min_idx
@@ -261,9 +285,10 @@ def compute_chunks(lat_min, lat_max, lon_min, lon_max):
     # Appending rows one by one in the while loop takes far more time then this.
     chunk_list_df = pd.DataFrame(data=image_list, columns=chunk_df_colums.keys())
     # Specify the schema.
-    chunk_list_df = chunk_list_df.astype(dtype = chunk_df_colums)
+    chunk_list_df = chunk_list_df.astype(dtype=chunk_df_colums)
     display_intermediate_time()
-    return (chunk_list_df, index_list, id_counter)
+    return chunk_list_df, index_list, id_counter
+
 
 def fetch_setting():
     import configparser
@@ -306,6 +331,7 @@ def fetch_setting():
             file_prefix, cyclone_lat_size, cyclone_lon_size, nb_proc, IS_DEBUG,
             has_save_results)
 
+
 # Allocation of the channels.
 # Making use of numpy array backended by ctypes shared array has been successfully
 # tested while in multiprocessing context.
@@ -313,6 +339,7 @@ def allocate_channel_array(id_counter):
     return np.ctypeslib.as_array(mp.RawArray(ctypes.ARRAY(ctypes.ARRAY(
         ctypes.ARRAY(ctypes.c_float, common.Y_RESOLUTION), common.X_RESOLUTION), id_counter),
         len(Era5)))
+
 
 def load_cnn_model(file_prefix):
     cnn_filename = f'{file_prefix}_{common.CNN_FILE_POSTFIX}.h5'
@@ -335,20 +362,20 @@ def compute_containing_region(chunk_list_df,recorded_cyclones,
         lat_max = lat+cyclone_lat_size
         lon_min = lon-cyclone_lat_size
         lon_max = lon+cyclone_lat_size
-        if is_intersection: # Using pandas or numpy intersection could be better.
-            current = (((chunk_list_df.lat_min < (lat_min)) & \
-                        (chunk_list_df.lat_max < (lat_max)))    | \
-                       ((chunk_list_df.lat_min > (lat_min))  & \
-                        (chunk_list_df.lat_max > (lat_max))))     & \
-                      (((chunk_list_df.lon_min < (lon_min)) & \
-                        (chunk_list_df.lon_max < (lon_max)))    | \
-                       ((chunk_list_df.lon_min > (lon_min))  & \
-                        (chunk_list_df.lon_max > (lon_max))))
+        if is_intersection:  # Using pandas or numpy intersection could be better.
+            current = (((chunk_list_df.lat_min < lat_min) &
+                        (chunk_list_df.lat_max < lat_max))    |
+                       ((chunk_list_df.lat_min > lat_min)  &
+                        (chunk_list_df.lat_max > lat_max)))     & \
+                      (((chunk_list_df.lon_min < lon_min) &
+                        (chunk_list_df.lon_max < lon_max))    |
+                       ((chunk_list_df.lon_min > lon_min)  &
+                        (chunk_list_df.lon_max > lon_max)))
         else:
-            current = (chunk_list_df.lat_min <= (lat_min)) & \
-                      (chunk_list_df.lat_max >= (lat_max)) & \
-                      (chunk_list_df.lon_min <= (lon_min)) & \
-                      (chunk_list_df.lon_max >= (lon_max))
+            current = (chunk_list_df.lat_min <= lat_min) & \
+                      (chunk_list_df.lat_max >= lat_max) & \
+                      (chunk_list_df.lon_min <= lon_min) & \
+                      (chunk_list_df.lon_max >= lon_max)
         if not current.any():
             nb_missing_recorded_cyclones = nb_missing_recorded_cyclones + 1
         if selected_serie is not None:
@@ -357,6 +384,7 @@ def compute_containing_region(chunk_list_df,recorded_cyclones,
         else:
             selected_serie = current
     return selected_serie, nb_missing_recorded_cyclones
+
 
 def prediction_analysis(file_prefix, channels_array, recorded_cyclones,
                         chunk_list_df, cyclone_lat_size, cyclone_lon_size,
@@ -412,19 +440,23 @@ def prediction_analysis(file_prefix, channels_array, recorded_cyclones,
     len_negatives = len(negatives)
     print(f'  > number of negatives: {len_negatives}')
 
-    false_positives = chunk_list_df[(chunk_list_df.pred_cat == common.CYCLONE_LABEL) & (chunk_list_df.true_cat == common.NO_CYCLONE_LABEL)]
+    false_positives = chunk_list_df[(chunk_list_df.pred_cat == common.CYCLONE_LABEL) &
+                                    (chunk_list_df.true_cat == common.NO_CYCLONE_LABEL)]
     len_false_positives = len(false_positives)
     print(f'  > model has {len_false_positives} false positives')
 
-    false_negatives = chunk_list_df[(chunk_list_df.pred_cat == common.NO_CYCLONE_LABEL) & (chunk_list_df.true_cat == common.CYCLONE_LABEL)]
+    false_negatives = chunk_list_df[(chunk_list_df.pred_cat == common.NO_CYCLONE_LABEL) &
+                                    (chunk_list_df.true_cat == common.CYCLONE_LABEL)]
     len_false_negatives = len(false_negatives)
     print(f'  > model has {len_false_negatives} false negatives')
 
-    true_positives = chunk_list_df[(chunk_list_df.pred_cat == common.CYCLONE_LABEL) & (chunk_list_df.true_cat == common.CYCLONE_LABEL)]
+    true_positives = chunk_list_df[(chunk_list_df.pred_cat == common.CYCLONE_LABEL) &
+                                   (chunk_list_df.true_cat == common.CYCLONE_LABEL)]
     len_true_positives = len(true_positives)
     print(f'  > model has {len_true_positives} true positives')
 
-    true_negatives = chunk_list_df[(chunk_list_df.pred_cat == common.NO_CYCLONE_LABEL) & (chunk_list_df.true_cat == common.NO_CYCLONE_LABEL)]
+    true_negatives = chunk_list_df[(chunk_list_df.pred_cat == common.NO_CYCLONE_LABEL) &
+                                   (chunk_list_df.true_cat == common.NO_CYCLONE_LABEL)]
     len_true_negatives = len(true_negatives)
     print(f'  > model has {len_true_negatives} true negatives')
 
@@ -442,12 +474,17 @@ def prediction_analysis(file_prefix, channels_array, recorded_cyclones,
     print(f'  > AUC: {common.format_pourcentage(auc_model)}%')
 
     print(f'  > metrics report:\n')
-    print(classification_report(y_true=chunk_list_df.true_cat, y_pred=y_pred_cat_npy, target_names=('no_cyclones', 'cyclones')))
+    print(classification_report(y_true=chunk_list_df.true_cat, y_pred=y_pred_cat_npy,
+                                target_names=['no_cyclones', 'cyclones']))
 
-    precision_cyclone    = precision_score(y_true=chunk_list_df.true_cat, y_pred=y_pred_cat_npy, pos_label=common.CYCLONE_LABEL)
-    precision_no_cyclone = precision_score(y_true=chunk_list_df.true_cat, y_pred=y_pred_cat_npy, pos_label=common.NO_CYCLONE_LABEL)
-    recall_cyclone    = recall_score(y_true=chunk_list_df.true_cat, y_pred=y_pred_cat_npy, pos_label=common.CYCLONE_LABEL)
-    recall_no_cyclone = recall_score(y_true=chunk_list_df.true_cat, y_pred=y_pred_cat_npy, pos_label=common.NO_CYCLONE_LABEL)
+    precision_cyclone    = precision_score(y_true=chunk_list_df.true_cat, y_pred=y_pred_cat_npy,
+                                           pos_label=common.CYCLONE_LABEL)
+    precision_no_cyclone = precision_score(y_true=chunk_list_df.true_cat, y_pred=y_pred_cat_npy,
+                                           pos_label=common.NO_CYCLONE_LABEL)
+    recall_cyclone    = recall_score(y_true=chunk_list_df.true_cat, y_pred=y_pred_cat_npy,
+                                     pos_label=common.CYCLONE_LABEL)
+    recall_no_cyclone = recall_score(y_true=chunk_list_df.true_cat, y_pred=y_pred_cat_npy,
+                                     pos_label=common.NO_CYCLONE_LABEL)
 
     metrics = (auc_model,
                precision_no_cyclone,
@@ -484,6 +521,7 @@ def save_results(cyclone_images_df, file_prefix, year, month, day, hour):
                                  line_terminator='\n')
         display_intermediate_time()
 
+
 def test(channels_array, chunk_list_df, index_list, netcdf_dict, day, hour,
          debug_lat, debug_lon):
     # Example:
@@ -501,23 +539,23 @@ def test(channels_array, chunk_list_df, index_list, netcdf_dict, day, hour,
     debug_lon_min = debug_lon - common.HALF_LON_FRAME
     debug_lon_max = debug_lon + common.HALF_LON_FRAME
 
-    record = chunk_list_df[(chunk_list_df.lat_min == debug_lat_min) & (chunk_list_df.lat_max == debug_lat_max) & (chunk_list_df.lon_min == debug_lon_min) & (chunk_list_df.lon_max == debug_lon_max)]
+    record = chunk_list_df[(chunk_list_df.lat_min == debug_lat_min) & (chunk_list_df.lat_max == debug_lat_max) &
+                           (chunk_list_df.lon_min == debug_lon_min) & (chunk_list_df.lon_max == debug_lon_max)]
     print(record)
     #       lat_min  lat_max  lon_min  lon_max          pred_prob  pred_cat
-    #30537     10.5     18.5   -37.25   -29.25  3.056721e-10       False
+    #       30537     10.5     18.5   -37.25   -29.25  3.056721e-10       False
     debug_id = record.index[0]
 
     print(f'indexes: {index_list[debug_id]}')
-    #print(f'recomputed indexes: lat_min_index = {latitude_indexes[debug_lat_max]} ; lat_max_index = {latitude_indexes[debug_lat_min]} ; lon_min_index = {longitude_indexes[debug_lon_min]} ; lon_max_index = {longitude_indexes[debug_lon_max]}')
 
     from matplotlib import pyplot as plt
     region = channels_array[variable.value.num_id][debug_id]
-    plt.imshow(region,cmap='gist_rainbow_r',interpolation="none")
+    plt.imshow(region, cmap='gist_rainbow_r', interpolation="none")
     plt.show()
 
     import extraction_utils as utils
     region = utils.extract_region(netcdf_dict[variable], variable, day, hour, debug_lat, debug_lon)
-    plt.imshow(region,cmap='gist_rainbow_r',interpolation="none")
+    plt.imshow(region, cmap='gist_rainbow_r', interpolation="none")
     plt.show()
 
     # The images must be the same, even if the image from the channels_array is based on
